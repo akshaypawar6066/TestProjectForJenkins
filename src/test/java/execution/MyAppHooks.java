4b17629204b03b484ab1af54353a07b230437010
Gourav <gourav@g.com>
2023-12-27 11:40:37 +0530

    .

--- src/test/java/execution/MyAppHooks.java
+++ src/test/java/execution/MyAppHooks.java
@@ -9,6 +9,10 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -52,24 +56,41 @@ public class MyAppHooks {
 		driver.manage().window().maximize();
 	}
 	
-	@After(order = 2)
-	public void tearDown(Scenario scenario) throws FileNotFoundException, IOException
+	public String getBase64Screenshot()
 	{
-		boolean isfailed = scenario.isFailed();
-		
-		if(isfailed)
-		{
-			String scenarioName = scenario.getName();
-			String name = scenarioName.replaceAll(" ", "_");
-			
-			TakesScreenshot ts = (TakesScreenshot)driver;
-			
-			byte[] source = ts.getScreenshotAs(OutputType.BYTES);
-			
-			scenario.attach(source, "image/png", name);
-			
-		}
+	    return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
 	}
+	
+	
+	@After(order = 2)
+	public void af(Scenario scenario) throws IOException, InterruptedException
+	{
+	    if(scenario.isFailed())
+	    {		       
+	        ExtentCucumberAdapter.getCurrentStep().log(Status.FAIL, MediaEntityBuilder.createScreenCaptureFromBase64String(getBase64Screenshot()).build());
+	    }
+		 
+	}
+	
+	
+//	@After(order = 2)
+//	public void tearDown(Scenario scenario) throws FileNotFoundException, IOException
+//	{
+//		boolean isfailed = scenario.isFailed();
+//		
+//		if(isfailed)
+//		{
+//			String scenarioName = scenario.getName();
+//			String name = scenarioName.replaceAll(" ", "_");
+//			
+//			TakesScreenshot ts = (TakesScreenshot)driver;
+//			
+//			byte[] source = ts.getScreenshotAs(OutputType.BYTES);
+//			
+//			scenario.attach(source, "image/png", name);
+//			
+//		}
+//	}
 	
 	@After(order = 1)
 	public void quitBrowser()
